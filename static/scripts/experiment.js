feb6b925e7a53269131034d5c1500cc685b15e2d
De-Wei David Ye <DeWeiYe6626@gmail.com>
2020-02-11 16:17:32 +0800

    adjust help page

--- static/scripts/experiment.js
+++ static/scripts/experiment.js
@@ -13,19 +13,6 @@ let countNormal = 0;
 let maxShow = 10;
 
 
-window.addEventListener("keydown", function (event) {
-    if (event.defaultPrevented) {
-        return; // Do nothing if the event was already processed
-    }
-
-    // if (event.key in keyMap) updateExperiment(event.key);
-    if (["Esc", "Escape"].includes(event.key)) window.location.href = '/';
-
-    // Cancel the default action to avoid it being handled twice
-    event.preventDefault();
-});
-
-
 document.querySelector("span.circle.red").addEventListener("click", function () {
     updateExperiment(experimentText[0])
 });
